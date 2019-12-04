fe1dc1a58ca6bd76d51601faf73684ceac624227
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2019-12-04 11:04:30 -0300

    instance socket normally

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ io.on('connection', socket => { // toda vez que um usuário se conectar
     socket.emit('previousMessages', messages); // manda os dados para todos clientes
 
     socket.on('sendMessage', data => {
-        console.log(data);
         messages.push(data); // armazena o objeto no array de mensagens
         socket.broadcast.emit('receivedMessage', data); 
     });
